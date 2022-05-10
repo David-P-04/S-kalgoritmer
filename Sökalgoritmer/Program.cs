746da28beb7586ec4cf9807fc8b9710c5e44acbc
David Persson <04DAPEA@skola.lidkoping.se>
2022-05-10 09:15:57 +0200

    Binärsök

--- Sökalgoritmer/Program.cs
+++ Sökalgoritmer/Program.cs
@@ -8,13 +8,13 @@ namespace Sökalgoritmer
         static void Main(string[] args)
         {
 
-            List<int> numbers = new List<int>();
+            /*List<int> numbers = new List<int>();
             Random r = new Random();
 
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 1000; i++)
             {
                 int randomnumber = r.Next(1, 100);
-                Console.Write($"{randomnumber} ,");
+                // Console.Write($"{randomnumber} ,");
                 numbers.Add(randomnumber);
             }
 
@@ -38,6 +38,40 @@ namespace Sökalgoritmer
 
                 return -1;
 
+            }*/
+
+            int[] numb = new int[100];
+            
+            for (int i = 0; i < 100; i++)
+            {
+                numb[i] = i;
+            }
+
+            Console.WriteLine("Vilket tal söker du?");
+            int sökttal = int.Parse(Console.ReadLine());
+
+            BinärSök(numb, sökttal);
+
+            static int BinärSök(int[] tal, int värde)
+            {
+                if (tal == null)
+                    return -1;
+
+                int start = 0;
+                int stop = tal.Length - 1;
+
+                while (start <= stop)
+                {
+                    int mitt = (start + stop) / 2;
+                    if (tal[mitt] < värde)
+                        start = mitt + 1;
+                    else if (tal[mitt] > värde)
+                        stop = mitt - 1;
+                    else
+                        return mitt;
+                }
+
+                return -1;
             }
 
         }
